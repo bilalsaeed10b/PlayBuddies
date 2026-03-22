b390b2773b7f82795230a786f84005bb8dc6e0be
bilal saeed <bilalsaeed10b@gmail.com>
2026-03-23 01:43:52 +0500

    .

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/PlayBuddies", // Adjusted for https://bilalsaeed10b.github.io/PlayBuddies/
+  images: {
+    unoptimized: true,
+  },
   serverExternalPackages: [],
 };
 
